292453a4d8c34faed2defb5e340fb1d3f2e498e2
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-01 22:28:39 +0200

    Add theme styles

---
+++ src/utilities/themeStyles/index.ts
@@ -0,0 +1,24 @@
+import { DefaultTheme } from 'styled-components';
+
+const themeStyles: DefaultTheme = {
+  colors: {
+    primary: 'rgba(81,42,163,1)',
+    secondary: 'rgba(246,166,0,1)',
+    gradient: 'linear-gradient(5deg, rgba(95,36,221,1) 0%, rgba(101,81,236,1) 100%)',
+    primaryText: 'rgba(55,50,67,1)',
+    grey1: 'rgba(179,179,179,1)',
+    grey2: 'rgba(214,214,214,1)',
+    grey3: 'rgba(232,232,232,1)',
+    grey4: 'rgba(250,250,250,1)',
+    white: 'rgba(255,255,255,1)',
+  },
+  shadows: {
+    dropShadow: '2px 2px 20px 0 rgba(61, 61, 61, 0.12)',
+  },
+  breakpoints: {
+    tablet: '768px',
+    desktop: '1280px',
+  },
+};
+
+export default themeStyles;
