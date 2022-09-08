853e6899f5cf66cdb7fafc81080f7121204a075d
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-08 17:02:30 +0200

    Create CardContainer component

---
+++ src/components/atoms/CardContainer/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+
+import { Container } from './styled'
+
+interface cardContainerProps {
+  className?: string,
+  children?: JSX.Element | JSX.Element[],
+  fullWidth?: boolean,
+}
+
+const CardContainer: FC<cardContainerProps> = ({ className, children, fullWidth }) => (
+  <Container className={className} fullWidth={fullWidth}>
+    {children}
+  </Container>
+)
+
+export default CardContainer
---
+++ src/components/atoms/CardContainer/styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+import themeStyles from "utilities/themeStyles"
+
+interface containerProps {
+  fullWidth?: boolean,
+}
+
+export const Container = styled.div<containerProps>`
+  grid-column: span 4;
+  background: ${themeStyles.colors.white};
+  border: 2px solid ${themeStyles.colors.grey3};
+  border-radius: 12px;
+  box-sizing: border-box;
+  padding: 32px 24px;
+
+  @media (min-width: ${themeStyles.breakpoints.desktop}) {
+    grid-column: span ${props => props.fullWidth ? 4 : 2};
+    padding: 60px;
+  }
+`
