f4e26337e5d9b79dcf749cdf393b3bb0b2e3e895
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-08 17:03:16 +0200

    Create GridContainer component

---
+++ src/components/atoms/GridContainer/index.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+import themeStyles from "utilities/themeStyles"
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
+
+  @media (min-width: ${themeStyles.breakpoints.desktop}) {
+    grid-gap: 40px;
+  }
+`
+
+export default GridContainer
