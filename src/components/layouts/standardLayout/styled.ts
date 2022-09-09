5deb40625092c40b005ac8464dd06cb5297d9b23
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-09 21:10:32 +0200

    Make style changes to StandardLayout component

--- src/components/layouts/standardLayout/styled.ts
+++ src/components/layouts/standardLayout/styled.ts
@@ -11,19 +11,23 @@ export const Container = styled.div<containerProps>`
   width: 100vw;
   height: 100%;
   margin-top: ${props => props.headerHeight}px;
-  padding: 40px 12px;
+  padding: 40px 12px 60px;
 
   @media (min-width: ${themeStyles.breakpoints.desktop}) {
     width: auto;
     margin-left: 360px;
-    padding: 80px;
+    padding: 80px 80px 120px;
   }
 `
 
 export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
   width: 100%;
 
   @media (min-width: ${themeStyles.breakpoints.desktop}) {
+    gap: 80px;
     height: 100%;
   }
 `
