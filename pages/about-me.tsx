fc3fe1194b7445a810f6955c960749c3c9f34721
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-09 21:10:10 +0200

    Populate 'about-me' page with components

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -6,6 +6,8 @@ import Head from 'next/head'
 import Layout from 'components/layouts/standardLayout'
 
 import AboutMeSection from 'components/organisms/AboutMeSection'
+import SkillsetSection from 'components/organisms/SkillsetSection'
+import CtaSection from 'components/molecules/CtaSection'
 
 
 const AboutMe: NextPageWithLayout = () => (
@@ -16,6 +18,12 @@ const AboutMe: NextPageWithLayout = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <AboutMeSection />
+    <SkillsetSection/>
+    <CtaSection 
+      title="Curious about my experience and education?"
+      buttonText="Take a look at my resume"
+      href="/resume"
+    />
   </>
 )
 
