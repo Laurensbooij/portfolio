92ae7ef2f810c0c7d87fa0835d627499ec490773
Laurensbooij <laurens_booij1@hotmail.com>
2022-09-19 12:44:25 +0200

    Add unordered list rendering to RichText component

--- src/components/atoms/RichText/index.tsx
+++ src/components/atoms/RichText/index.tsx
@@ -3,7 +3,7 @@ import parse from 'html-react-parser'
 
 import { H1, H2, H3, H4, H5, H6, BodyText } from '../TextElements'
 
-import { Container } from './styled'
+import { Container, Ul } from './styled'
 
 interface richTextProps {
   content: any,
@@ -11,12 +11,10 @@ interface richTextProps {
 
 const RichText: FC<richTextProps> = ({ content }) => {
   
-  const stringExtractionHandler = (content: any) => {  
-    console.log(content)
+  const extractText = (content: any) => {  
 
     let stringsArray = content.map((item: any) => {
       if(item.marks.find((mark: any) => mark.type === 'bold')) {
-        console.log(item)
         return `<b>${item.value}</b>`
       }
 
@@ -33,29 +31,43 @@ const RichText: FC<richTextProps> = ({ content }) => {
     return element
   }
 
+  const generateListItems = (content: any) => {
+
+    
+    
+    const listItems = content.map((item: any, index: number) => <li key={`${index}-${item.nodetype}`}>{extractText(item.content[0].content)}</li>)
+    return listItems
+  }
+
   let formattedContent = content.map((node: any, index: number) => {
 
     switch(node.nodeType) {
       case 'heading-1':
-        return <H1 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H1>
+        return <H1 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H1>
         break
       case 'heading-2':
-        return <H2 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H2>
+        return <H2 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H2>
         break
       case 'heading-3':
-        return <H3 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H3>
+        return <H3 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H3>
         break
       case 'heading-4':
-        return <H4 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H4>
+        return <H4 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H4>
         break
       case 'heading-5':
-        return <H5 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H5>
+        return <H5 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H5>
         break
       case 'heading-6':
-        return <H6 key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</H6>
+        return <H6 key={`${index}-${node.nodetype}`}>{extractText(node.content)}</H6>
+        break
+      case 'unordered-list':
+        const listItems = generateListItems(node.content)
+        return (
+          <Ul key={`${index}-${node.nodetype}`}>{listItems}</Ul>
+        )
         break
       default:
-        return <BodyText key={`${index}-${node.nodetype}`}>{stringExtractionHandler(node.content)}</BodyText>
+        return <BodyText key={`${index}-${node.nodetype}`}>{extractText(node.content)}</BodyText>
     }
   })
   
