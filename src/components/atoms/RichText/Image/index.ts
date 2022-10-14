83e5de2de6304bb795b51b5ca7a6be2eeb36329f
Laurensbooij <laurens_booij1@hotmail.com>
2022-10-14 13:27:13 +0200

    Change image size and scaling behaviour

--- src/components/atoms/RichText/Image/index.ts
+++ src/components/atoms/RichText/Image/index.ts
@@ -6,14 +6,18 @@ const Image = styled.img`
   display: block;
   height: 200px;
   width: 100%;
-  margin: 32px 0;
+  margin: 40px 0;
   border: 2px solid ${themeStyles.colors.grey3};
   border-radius: 12px;
-  object-fit: none;
+  object-fit: cover;
+
+  @media (min-width: ${themeStyles.breakpoints.tablet}) {
+    height: 320px;
+  }
 
   @media (min-width: ${themeStyles.breakpoints.desktop}) {
-    height: 240px;
-    margin: 40px 0;
+    height: 360px;
+    margin: 60px 0;
   }
 `
 
