c2cf1e37f37ebb97e685ea494787ac1cd59ebf21
Laurensbooij <laurens_booij1@hotmail.com>
2022-10-22 12:00:40 +0200

    Change margins in RichText

--- src/components/atoms/TextElements/index.ts
+++ src/components/atoms/TextElements/index.ts
@@ -7,8 +7,12 @@ export const H1 = styled.h1`
   font-weight: 600;
   font-size: 1.75rem;
   line-height: 140%;
-  margin: 0 0 1rem;
+  margin: 3rem 0 0.5rem;
   color: ${themeStyles.colors.primaryText};
+
+  + h2, + h3, + h4, + h5, + h6 {
+    margin: 2rem 0 0.5rem;
+  }
 
   @media (min-width: ${themeStyles.breakpoints.tablet}) {
     font-size: 3rem;
@@ -21,8 +25,12 @@ export const H2 = styled.h2`
   font-weight: 600;
   font-size: 1.5rem;
   line-height: 140%;
-  margin: 0 0 0.75rem;
+  margin: 2.5rem 0 0.5rem;
   color: ${themeStyles.colors.primaryText};
+
+  + h3, + h4, + h5, + h6 {
+    margin: 1.875rem 0 0.5rem;
+  }
 
   @media (min-width: ${themeStyles.breakpoints.tablet}) {
     font-size: 2rem;
@@ -35,13 +43,18 @@ export const H3 = styled.h3`
   font-weight: 600;
   font-size: 1.375rem;
   line-height: 140%;
-  margin: 0 0 0.5rem;
+  margin: 2.25rem 0 0.5rem;
   color: ${themeStyles.colors.primaryText};
+
+  + h4, + h5, + h6 {
+    margin: 1.25rem 0 0.5rem;
+  }
 
   @media (min-width: ${themeStyles.breakpoints.tablet}) {
     font-size: 1.5rem;
     margin: 2.25rem 0 0.5rem;
   }
+
 `
 
 export const H4 = styled.h4`
@@ -49,11 +62,14 @@ export const H4 = styled.h4`
   font-weight: 600;
   font-size: 1.125rem;
   line-height: 140%;
-  margin: 0 0 0.375rem;
+  margin: 2.125rem 0 0.5rem;
   color: ${themeStyles.colors.primaryText};
 
+  + h5, + h6 {
+    margin: 1.125rem 0 0.5rem;
+  }
+
   @media (min-width: ${themeStyles.breakpoints.tablet}) {
-    font-size: 1.25rem;
     margin: 2.125rem 0 0.5rem;
   }
 `
@@ -61,13 +77,16 @@ export const H4 = styled.h4`
 export const H5 = styled.h5`
   font-family: 'Rubik', 'Montserrat', Roboto, Helvetica Neue, sans-serif;
   font-weight: 600;
-  font-size: 1.125rem;
+  font-size: 1rem;
   line-height: 160%;
-  margin: 0 0 0.25rem;
+  margin: 2rem 0 0.5rem;
   color: ${themeStyles.colors.primaryText};
 
+  + h6 {
+    margin: 1rem 0 0.5rem;
+  }
+
   @media (min-width: ${themeStyles.breakpoints.tablet}) {
-    font-size: 1rem;
     margin: 2rem 0 0.5rem;
   }
 `
@@ -78,7 +97,7 @@ export const H6 = styled.h6`
   font-size: 1rem;
   line-height: 160%;
   margin: 1.75rem 0 0.5rem;
-  color: ${themeStyles.colors.primaryText};
+  color: ${themeStyles.colors.grey1};
 `
 
 const hoverAnimation = keyframes`
